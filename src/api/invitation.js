36b14ddd30884d491cf2a7ecd5dc36d7ff52adc2
amysunny112 <914531319@qq.com>
2021-07-19 23:07:42 +0800

    add;invitation

---
+++ src/api/invitation.js
@@ -0,0 +1,6 @@
+
+// 邀请
+export const collect_add = {
+    path: '/web/infoCollect/add',
+    method: 'post'
+}
--- src/views/Invitation.vue
+++ src/views/Invitation.vue
@@ -6,44 +6,94 @@
         <h4>Here we need some information about cooperation preferences:</h4>
         <Form :model="form" label-position="top">
           <FormItem label="1.What is the compensation rate?">
-            <Input
+            <!-- <Input
               v-for="(item, index) in formItem"
               :key="index"
-              v-model="form.input1"
+              v-model="item.value"
+              style="margin-bottom: 10px"
+            > 
+              <div slot="prepend" class="label">{{ item.name }}</div>
+              <Button slot="append">USB</Button>
+            </Input> -->
+            <Input
+              v-model="form.youTuBe"
+              type="number"
               style="margin-bottom: 10px"
             >
-              <div slot="prepend" class="label">{{ item.name }}</div>
+              <div slot="prepend" class="label">YouTube</div>
+              <Button slot="append">USB</Button>
+            </Input>
+            <Input
+              v-model="form.instagram"
+              type="number"
+              style="margin-bottom: 10px"
+            >
+              <div slot="prepend" class="label">Instagram</div>
+              <Button slot="append">USB</Button>
+            </Input>
+            <Input
+              v-model="form.tikTok"
+              type="number"
+              style="margin-bottom: 10px"
+            >
+              <div slot="prepend" class="label">TikTok</div>
+              <Button slot="append">USB</Button>
+            </Input>
+            <Input
+              v-model="form.twitter"
+              type="number"
+              style="margin-bottom: 10px"
+            >
+              <div slot="prepend" class="label">Twitter</div>
+              <Button slot="append">USB</Button>
+            </Input>
+            <Input
+              v-model="form.twich"
+              type="number"
+              style="margin-bottom: 10px"
+            >
+              <div slot="prepend" class="label">Twitch</div>
+              <Button slot="append">USB</Button>
+            </Input>
+            <Input
+              v-model="form.pinterest"
+              type="number"
+              style="margin-bottom: 10px"
+            >
+              <div slot="prepend" class="label">Pinterest</div>
               <Button slot="append">USB</Button>
             </Input>
             <span>* other social media platforms.</span>
             <Input
-              v-model="form.input1"
+              v-model="form.otherPlatform"
               style="margin-bottom: 10px"
               placeholder="name"
             >
             </Input>
-            <Input v-model="form.input1">
+            <Input v-model="form.otherPlatformRate" type="number">
               <Button slot="append">USB</Button>
             </Input>
           </FormItem>
           <FormItem label="2.Accept product/sample delivery,or affiliate ways?">
-            <RadioGroup v-model="form.gender">
-              <Radio label="male">Yes</Radio>
-              <Radio label="female">No</Radio>
+            <RadioGroup v-model="form.accept">
+              <Radio :label="1">Yes</Radio>
+              <Radio :label="0">No</Radio>
             </RadioGroup>
           </FormItem>
           <FormItem
             label="3.Specific product or industry you are interested in cooperating with:"
           >
             <Input
-              v-model="form.desc"
+              v-model="form.infoCollectcol"
               type="textarea"
               :autosize="{ minRows: 3, maxRows: 8 }"
               placeholder="E.g:cosmetics,clothes,tech Evs or gaming and so on"
             ></Input>
           </FormItem>
           <FormItem style="text-align: center">
-            <Button type="info" @click="handleSubmit">Submit</Button>
+            <Button type="info" @click="handleSubmit" :disabled="disabled">{{
+              disabled ? "Submitted" : "Submit"
+            }}</Button>
           </FormItem>
         </Form>
       </div>
@@ -53,41 +103,85 @@
 </template>
 
 <script>
+import { collect_add } from "../api/invitation";
 export default {
   name: "Invitation",
   data() {
     return {
       form: {
-        input1: "",
-        gender: "",
-        desc: "",
+        youTuBe: "",
+        instagram: "",
+        tikTok: "",
+        twitter: "",
+        twich: "",
+        pinterest: "",
+        otherPlatform: "",
+        otherPlatformRate: "",
+        accept: 1,
+        infoCollectcol: "",
       },
+      disabled: false,
+      email: "",
       formItem: [
         {
           name: "YouTube",
+          value: "youTuBe",
         },
         {
           name: "Instagram",
+          value: "instagram",
         },
         {
           name: "TikTok",
+          value: "tikTok",
         },
         {
           name: "Twitter",
+          value: "twitter",
         },
         {
           name: "Twitch",
+          value: "twich",
         },
         {
           name: "Pinterest",
+          value: "pinterest",
         },
       ],
     };
   },
   created() {},
   computed: {},
+  mounted() {},
   methods: {
-    handleSubmit() {},
+    handleSubmit() {
+      this.form.email = this.$route.query.email;
+      this.$httpRequest({
+        api: collect_add,
+        data: this.form,
+        success: (res) => {
+          console.log(res);
+          if (res.code == 0) {
+            this.$Message.success(res.message);
+            this.disabled = true;
+            this.form = {
+              youTuBe: "",
+              instagram: "",
+              tikTok: "",
+              twitter: "",
+              twich: "",
+              pinterest: "",
+              otherPlatform: "",
+              otherPlatformRate: "",
+              accept: 1,
+              infoCollectcol: "",
+            };
+          } else {
+            this.$Message.info(res.message);
+          }
+        },
+      });
+    },
   },
 };
 </script>
