02c037f17496b03d7a41d0042ee25d6a2fcc97eb
amysunny112 <914531319@qq.com>
2021-07-29 21:02:11 +0800

    营销计划接口对接

---
+++ src/api/collaborator.js
@@ -0,0 +1,5 @@
+// 合伙人列表
+export const usersList = {
+    path: '/web/users/list',
+    method: 'get'
+}
--- src/views/Collaborator.vue
+++ src/views/Collaborator.vue
@@ -1,428 +1,334 @@
 <template>
   <div class="collaborator">
-
     <!-- 搜索 -->
     <div class="search">
       <div>
         <span>合作者姓名</span>
-        <Input v-model="value" size="large" clearable class="width" />
+        <Input v-model="form.realName" size="large" clearable class="width" />
       </div>
       <div>
         <span>合作者ID</span>
-        <Input v-model="value" size="large" clearable class="width" />
+        <Input v-model="form.id" size="large" clearable class="width" />
       </div>
       <div>
         <span>合作者类型</span>
-        <Select v-model="model1" size="large" clearable class="width">
-          <Option :value="1">私域</Option>
-          <Option :value="2">公域</Option>
+        <Select v-model="form.userType" size="large" clearable class="width">
+          <Option value="">全部</Option>
+          <Option value="私域">私域</Option>
+          <Option value="公域">公域</Option>
         </Select>
       </div>
       <div>
         <span>语言</span>
-        <Select v-model="model1" size="large" clearable class="width">
-          <Option :value="1">English</Option>
-          <Option :value="2">German</Option>
-          <Option :value="3">French</Option>
-          <Option :value="4">Spanish</Option>
-          <Option :value="5">Portuguese</Option>
-          <Option :value="6">Ltalian</Option>
-          <Option :value="7">Russian</Option>
-          <Option :value="8">Japanese</Option>
-          <Option :value="9">Korean</Option>
-          <Option :value="10">Polish</Option>
-          <Option :value="11">Dutch</Option>
-          <Option :value="12">Czech</Option>
-          <Option :value="13">Danish</Option>
-          <Option :value="14">Finnish</Option>
-          <Option :value="15">Lrish</Option>
-          <Option :value="16">Hungarian</Option>
-          <Option :value="17">Armenian</Option>
-          <Option :value="18">Turkish</Option>
-          <Option :value="19">Vietnamese</Option>
-          <Option :value="20">Thai</Option>
-          <Option :value="21">Arabic</Option>
-          <Option :value="22">Hebrew</Option>
-          <Option :value="23">Lndonesian</Option>
-          <Option :value="24">Malay</Option>
+        <Select v-model="form.language" size="large" clearable class="width">
+          <Option value="">全部</Option>
+          <Option value="英语">英语</Option>
+          <Option value="日语">日语</Option>
+          <Option value="中文">中文</Option>
         </Select>
       </div>
       <div>
-        <span>国家</span>
-        <Select v-model="model1" size="large" clearable class="width">
-          <Option :value="1">Germany</Option>
+        <span>地区</span>
+        <Select v-model="form.country" size="large" clearable class="width">
+          <Option value="">全部</Option>
+          <Option value="欧美地区">欧美地区</Option>
+          <Option value="日本">日本</Option>
+          <Option value="韩国">韩国</Option>
+          <Option value="澳大利亚">澳大利亚</Option>
         </Select>
       </div>
       <div>
         <span>类别</span>
-        <Select v-model="model1" size="large" clearable class="width">
-          <Option :value="1">黑名单</Option>
-          <Option :value="2">白名单</Option>
+        <Select v-model="form.userClass" size="large" clearable class="width">
+          <Option value="">全部</Option>
+          <Option value="黑名单">黑名单</Option>
+          <Option value="白名单">白名单</Option>
         </Select>
       </div>
       <div>
-        <Button type="info" ghost class="button" size="large">重置</Button>
-        <Button type="info" class="button" size="large">查询</Button>
+        <Button
+          type="info"
+          ghost
+          class="button"
+          size="large"
+          @click="handleReset"
+          >重置</Button
+        >
+        <Button type="info" class="button" size="large" @click="handleSearch"
+          >查询</Button
+        >
       </div>
     </div>
 
     <!-- 表格 -->
     <div class="table">
-      <Tabs value="name1">
+      <Tabs :value="tabsValue" @on-click="handleTabs">
         <TabPane label="全部" name="name1">
-            <Table :columns="columns" :data="data"></Table>
-            <Page :total="100" show-sizer class="page"/>
+          <Table :columns="columns" :data="data">
+            <template slot-scope="{ row, index }" slot="action">
+              <Button type="info" @click="details(row, index)">查看</Button>
+            </template>
+          </Table>
+          <Page
+            :total="total"
+            :current="pageNo"
+            :page-size="pageSize"
+            @on-change="changePage"
+            @on-page-size-change="changeSize"
+            show-sizer
+            class="page"
+          />
         </TabPane>
-        <TabPane label="白名单" name="name2">
-            <Table :columns="columns" :data="data1"></Table>
-            <Page :total="100" show-sizer class="page"/>
+        <TabPane label="白名单" name="白名单">
+          <Table :columns="columns" :data="data">
+            <template slot-scope="{ row, index }" slot="action">
+              <Button type="info" @click="details(row, index)">查看</Button>
+            </template>
+          </Table>
+          <Page
+            :total="total"
+            :current="pageNo"
+            :page-size="pageSize"
+            @on-change="changePage"
+            @on-page-size-change="changeSize"
+            show-sizer
+            class="page"
+          />
         </TabPane>
-        <TabPane label="黑名单" name="name3">
-            <Table :columns="columns" :data="data2"></Table>
-            <Page :total="100" show-sizer class="page"/>
+        <TabPane label="黑名单" name="黑名单">
+          <Table :columns="columns" :data="data">
+            <template slot-scope="{ row, index }" slot="action">
+              <Button type="info" @click="details(row, index)">查看</Button>
+            </template>
+          </Table>
+          <Page
+            :total="total"
+            :current="pageNo"
+            :page-size="pageSize"
+            @on-change="changePage"
+            @on-page-size-change="changeSize"
+            show-sizer
+            class="page"
+          />
         </TabPane>
-    </Tabs>
+      </Tabs>
     </div>
+    <!-- 详情 -->
+    <Modal title="查看详情" v-model="modal" width="700">
+      <Form :model="detail" label-position="right" :label-width="90">
+        <Row class="modal_row">
+          <Col span="12">
+            <FormItem label="合作者ID">
+              <span>{{ detail.id }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="姓名">
+              <span>{{ detail.realName }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="国家">
+              <span>{{ detail.country }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="邮件">
+              <span>{{ detail.email }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="即时通讯工具">
+              <span>{{ detail.imno }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="语言">
+              <span>{{ detail.language }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="logo">
+              <img
+                :src="detail.logo"
+                style="width: 50px; height: 50px"
+              /> </FormItem
+          ></Col>
+          <Col span="12">
+            <FormItem label="手机">
+              <span>{{ detail.phone }}</span>
+            </FormItem></Col
+          >
+
+          <Col span="12">
+            <FormItem label="店铺名称">
+              <span>{{ detail.shopName }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="店铺地址">
+              <span>{{ detail.shopUrl }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="类别">
+              <span>{{ detail.userClass }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="类型">
+              <span>{{ detail.userType }}</span>
+            </FormItem></Col
+          >
+          <Col span="12">
+            <FormItem label="网站名称">
+              <span>{{ detail.websiteName }}</span>
+            </FormItem></Col
+          >
+          <Col span="24">
+            <FormItem label="描述">
+              <span>{{ detail.remark }}</span>
+            </FormItem></Col
+          >
+        </Row>
+      </Form>
+    </Modal>
   </div>
 </template>
 <script>
+import { usersList } from "../api/collaborator";
 export default {
   name: "Collaborator",
   data() {
     return {
-      value: "",
-      model1: "",
-      countryList:[
-        {
-          value:1,
-          name:'Germany'
-        },{
-          value:1,
-          name:'France'
-        },{
-          value:1,
-          name:'Italy'
-        },{
-          value:1,
-          name:'Spain'
-        },{
-          value:1,
-          name:'Portugal'
-        },{
-          value:1,
-          name:'United States'
-        },{
-          value:1,
-          name:'United Kingdom'
-        },{
-          value:1,
-          name:'Brazil'
-        },{
-          value:1,
-          name:'Japan'
-        },{
-          value:1,
-          name:'Russian Federation'
-        },{
-          value:1,
-          name:'Poland'
-        },{
-          value:1,
-          name:'Israel'
-        },{
-          value:1,
-          name:'Czech Republic'
-        },{
-          value:1,
-          name:'Slovakia'
-        },{
-          value:1,
-          name:'Hungary'
-        },{
-          value:1,
-          name:'Netherlands'
-        },{
-          value:1,
-          name:'Vietnam'
-        },{
-          value:1,
-          name:'Thailand'
-        },{
-          value:1,
-          name:'Turkey'
-        },{
-          value:1,
-          name:'Mexico'
-        },{
-          value:1,
-          name:'Korea, Republic of'
-        },{
-          value:1,
-          name:'Afghanistan'
-        },{
-          value:1,
-          name:'Åland Islands'
-        },{
-          value:1,
-          name:'Albania'
-        },{
-          value:1,
-          name:'Algeria'
-        },{
-          value:1,
-          name:'American Samoa'
-        },{
-          value:1,
-          name:'Andorra'
-        },{
-          value:1,
-          name:'Angola'
-        },{
-          value:1,
-          name:'Anguilla'
-        },{
-          value:1,
-          name:'Antarctica'
-        },{
-          value:1,
-          name:'Antigua and Barbuda'
-        },{
-          value:1,
-          name:'Argentina'
-        },{
-          value:1,
-          name:'Armenia'
-        },{
-          value:1,
-          name:'Aruba'
-        },{
-          value:1,
-          name:'Australia'
-        },{
-          value:1,
-          name:'Austria'
-        },{
-          value:1,
-          name:'Azerbaijan'
-        },{
-          value:1,
-          name:'Bahamas'
-        },{
-          value:1,
-          name:'Bahrain'
-        },{
-          value:1,
-          name:'Bangladesh'
-        },{
-          value:1,
-          name:'Barbados'
-        },{
-          value:1,
-          name:'Belarus'
-        },{
-          value:1,
-          name:'Belgium'
-        },{
-          value:1,
-          name:'Belize'
-        },{
-          value:1,
-          name:'Benin'
-        },{
-          value:1,
-          name:'Bermuda'
-        },{
-          value:1,
-          name:'Bhutan'
-        },{
-          value:1,
-          name:'Bolivia, Plurinational State of'
-        },{
-          value:1,
-          name:'Bonaire, Sint Eustatius and Saba'
-        },{
-          value:1,
-          name:'Bosnia and Herzegovina'
-        },{
-          value:1,
-          name:'Botswana'
-        },{
-          value:1,
-          name:'Bouvet Island'
-        },{
-          value:1,
-          name:'British Indian Ocean Territory'
-        },{
-          value:1,
-          name:'Brunei Darussalam'
-        },{
-          value:1,
-          name:'Bulgaria'
-        },{
-          value:1,
-          name:'Burkina Faso'
-        },{
-          value:1,
-          name:'Burundi'
-        },{
-          value:1,
-          name:'Cambodia'
-        },{
-          value:1,
-          name:'Cameroon'
-        },{
-          value:1,
-          name:'Canada'
-        },{
-          value:1,
-          name:'Cape Verde'
-        },{
-          value:1,
-          name:'Cayman Islands'
-        },{
-          value:1,
-          name:'Central African Republic'
-        },{
-          value:1,
-          name:'Chad'
-        },{
-          value:1,
-          name:'Chile'
-        },{
-          value:1,
-          name:'China'
-        },{
-          value:1,
-          name:'Christmas Island'
-        },{
-          value:1,
-          name:'Cocos (Keeling) Islands'
-        },{
-          value:1,
-          name:'Colombia'
-        },{
-          value:1,
-          name:'Comoros'
-        },{
-          value:1,
-          name:'Congo'
-        },{
-          value:1,
-          name:'Congo, the Democratic Republic of the'
-        },{
-          value:1,
-          name:'Cook Islands'
-        },{
-          value:1,
-          name:'Costa Rica'
-        },{
-          value:1,
-          name:"Côte d'Ivoire"
-        },{
-          value:1,
-          name:'Croatia'
-        },{
-          value:1,
-          name:'Cuba'
-        },{
-          value:1,
-          name:'Curaçao'
-        },{
-          value:1,
-          name:'Cyprus'
-        },{
-          value:1,
-          name:'Denmark'
-        },{
-          value:1,
-          name:'Djibouti'
-        },{
-          value:1,
-          name:'Dominica'
-        },{
-          value:1,
-          name:'Dominican Republic'
-        },{
-          value:1,
-          name:'Ecuador'
-        },{
-          value:1,
-          name:'Egypt'
-        },{
-          value:1,
-          name:'El Salvador'
-        },{
-          value:1,
-          name:'Equatorial Guinea'
-        },{
-          value:1,
-          name:'Eritrea'
-        },{
-          value:1,
-          name:'Estonia'
-        },{
-          value:1,
-          name:'Ethiopia'
-        },{
-          value:1,
-          name:'Falkland Islands (Malvinas)'
-        },{
-          value:1,
-          name:'Faroe Islands'
-        },{
-          value:1,
-          name:'Fiji'
-        },{
-          value:1,
-          name:'Finland'
-        },{
-          value:1,
-          name:'French Guiana'
-        },{
-          value:1,
-          name:'French Polynesia'
-        }
-      ],
-      columns:[
+      tabsValue: "name1",
+      modal: false,
+      form: {
+        realName: "",
+        id: "",
+        userType: "",
+        language: "",
+        country: "",
+        userClass: "",
+      },
+      columns: [
         {
           title: "合作者ID",
-          key: "name",
+          key: "id",
+          align: "center",
         },
         {
           title: "姓名",
-          key: "age",
+          key: "realName",
+          align: "center",
         },
         {
           title: "国家",
-          key: "address",
+          key: "country",
+          align: "center",
         },
         {
           title: "语言",
-          key: "address",
+          key: "language",
+          align: "center",
         },
         {
           title: "类型",
-          key: "address",
+          key: "userType",
+          align: "center",
         },
         {
           title: "类别",
-          key: "address",
+          key: "userClass",
+          align: "center",
         },
         {
           title: "操作",
-          key: "address",
+          slot: "action",
+          align: "center",
         },
       ],
-      data:[],
-      data1:[],
-      data2:[]
+      data: [],
+      pageNo: 1, //页数
+      pageSize: 10, //条数
+      total: 0, //总条数
+      data1: [],
+      data2: [],
+      userRole: 2, //0:管理员1:普通用户2:网红
+      detail: {},
     };
+  },
+  created() {
+    this.initData();
+  },
+  methods: {
+    // 初始化数据
+    initData() {
+      let data = {
+        pageNo: this.pageNo,
+        pageSize: this.pageSize,
+        userRole: this.userRole,
+        ...this.form,
+      };
+      this.$httpRequest({
+        api: usersList,
+        data,
+        success: (res) => {
+          console.log(res);
+          this.data = res.result.records;
+          this.total = res.result.total;
+        },
+      });
+    },
+    // 改变页数
+    changePage(page) {
+      console.log(page);
+      this.pageNo = page;
+      this.initData();
+    },
+    // 改变条数
+    changeSize(size) {
+      console.log(size);
+      this.pageSiz = size;
+      this.initData();
+    },
+    // 搜索
+    handleSearch() {
+      this.initData();
+    },
+    //重置
+    handleReset() {
+      this.form = {
+        realName: "",
+        id: "",
+        userType: "",
+        language: "",
+        country: "",
+        userClass: "",
+      };
+      this.initData();
+    },
+    //查看详情
+    details(row) {
+      this.modal = true;
+      this.detail = row;
+      console.log(row);
+    },
+    // tabs切换
+    handleTabs(ev) {
+      console.log(ev);
+      if (ev == "name1") {
+        this.form.userClass = "";
+      } else {
+        this.form.userClass = ev;
+      }
+      this.initData();
+    },
   },
 };
 </script>
 <style lang="less" scoped>
 .collaborator {
-
   .search {
     background-color: #ffffff;
     border-radius: 4px;
@@ -487,11 +393,18 @@ export default {
         }
       }
     }
-    .page{
+    .page {
       text-align: right;
       margin-top: 30px;
       padding-bottom: 30px;
     }
+  }
+}
+.modal_row {
+  span {
+    color: #333;
+    font-weight: bold;
+    font-size: 15px;
   }
 }
 </style>
